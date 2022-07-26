258a3acb545c8953f4bab69f53cce0bc7f37a465
NICOLE\Nicole <annicole_rosas@outlook.com>
2022-07-26 12:35:32 -0500

    add async method

--- solution/async-sorted-merge.js
+++ solution/async-sorted-merge.js
@@ -2,12 +2,40 @@
 
 // Print all entries, across all of the *async* sources, in chronological order.
 
-module.exports = (logSources, printer) => {
-  return new Promise((resolve, reject) => {
-    for(let i=0;i<3;i++){
-      let logEntry= logSources[i].popAsync();
-      printer.print(logEntry);
-     }
-    resolve(console.log("Async sort complete."));
-  });
+const initializeArray = async (logSources) => {
+   const array = await Promise.all( logSources.map( (logSource) => logSource.popAsync()));
+   return array;
+};
+
+const printNextAndGetIndex = (array, printer) => {
+  try {
+    let next = null;
+    let nextLogEntry = null;
+    let lowestIndex = null;
+
+    for (const [index, entry] of array.entries()) {
+      if (entry && (entry.date.getTime() < next || next === null)) {
+        next = entry.date.getTime();
+        lowestIndex = index;
+        nextLogEntry = entry;
+      }
+    }
+    printer.print(nextLogEntry);
+    return lowestIndex;
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+const checkIfDone = (array) => {
+  return array.every((entry) => entry === false);
+};
+
+module.exports = async (logSources, printer) => {
+  const array = await initializeArray(logSources);
+  while (!checkIfDone(array)) {
+    const index = printNextAndGetIndex(array, printer);
+    array[index] = await logSources[index].popAsync();
+  }
+  printer.done();
 };
